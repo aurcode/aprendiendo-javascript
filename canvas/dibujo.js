41c93c613b4e92d7d14c3898e16575e2800df041
aurcode <63828066+aurcode@users.noreply.github.com>
2020-05-25 08:30:15 -0400

    Usuario puede escoger cantidad de lineas
    Agregue un formulario y un evento en el cual el usuario colocara el numero de lineas con las que quiere crear la figura, y en base al numero crear la figura con más o menos líneas.

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,9 +1,10 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick );
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d"); 
-var lineas = 30
-var l = 1
-var yi, xf;
-var xi, yf;
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -15,22 +16,26 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 	lienzo.closePath();
 }
 
-for (l = 0; l < lineas; l++ )
-{
-	yi = 10 * l;
-	xf = 10 * (1 + l);
-	dibujarLinea("red", 0, yi, xf, 300);
-}
-
-for (l = 0; l < lineas; l++ )
-{
-	xi = 10 * l;
-	yf = 10 * (1 + l);
-	dibujarLinea("red", xi, 0, 300, yf);
-}
-
-
+linea_ancho = ancho - 1
 dibujarLinea("black", 1, 1, 1, 299);
 dibujarLinea("black", 300, 1, 1, 1);
 dibujarLinea("black", 299, 1, 299, 299);
 dibujarLinea("black", 299, 299, 1, 299);
+
+function dibujoPorClick()
+{
+	var lineas = parseInt(texto.value);
+	var l = 1;
+	var yi, xf;
+	var xi, yf;
+	var espacio = ancho / lineas
+
+for (l = 0; l < lineas; l++ )
+{
+	yi = espacio * l;
+	xf = espacio * (1 + l);
+	dibujarLinea("red", 0, yi, xf, 300);
+}
+
+}
+
