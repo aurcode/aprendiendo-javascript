c571c7a172dcda5a95b497a06de99966cc3fe182
aurcode <63828066+aurcode@users.noreply.github.com>
2020-05-26 05:32:57 -0400

    Dibujar en canvas con teclado
    Pagina en fase de desarrollo, la cual se puede dibujar lineas rectas con las flechas del teclado.

--- flechas/eventos.js
+++ flechas/eventos.js
@@ -8,18 +8,59 @@ var teclas =
 
 console.log(teclas)
 
-document.addEventListener("keyup", dibujarTeclado);
+document.addEventListener("keydown", dibujarTeclado);
+
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+
+dibujarLinea("red", 5, 149, 149, 151, 151, papel);
+
+function dibujarLinea(color, ancho, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.lineWidth = ancho;
+	lienzo.moveTo(xinicial, yinicial);
+	lienzo.lineTo(xfinal, yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
+}
 
 function dibujarTeclado(evento)
 {
-	if(evento.keyCode == teclas.UP)
-	{
-		console.log("vamos pa riba")
-	}
+	var colorcito = "blue"
+	var anchito = 3
+	var movimiento = 1
 
-	if(evento.keyCode == teclas.DOWN)
+	switch(evento.keyCode)
 	{
-		console.log("vamos pa bajo")
-	}
+		
+		case teclas.UP:
+			dibujarLinea(colorcito, anchito, x, y, x, y - movimiento, papel);
+			y = y - movimiento;
+		break;
 
+		case teclas.DOWN:
+			dibujarLinea(colorcito, anchito, x, y, x, y + movimiento, papel);
+			y = y + movimiento;
+		break;
+
+		case teclas.LEFT:
+			dibujarLinea(colorcito, anchito, x, y, x - movimiento, y, papel);
+			x = x - movimiento;
+	
+		break;
+
+		case teclas.RIGHT:
+			dibujarLinea(colorcito, anchito, x, y, x + movimiento, y, papel);
+			x = x + movimiento;
+		break;
+
+		default:
+			console.log("Otra tecla");
+		break;
+
+	}
 }
