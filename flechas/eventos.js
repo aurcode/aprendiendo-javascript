befe2621693e7e5e4c44c0e90ab1b218628d9e6a
aurcode <63828066+aurcode@users.noreply.github.com>
2020-05-26 16:58:27 -0400

    Mejora al codigo para que sea más legible

--- flechas/eventos.js
+++ flechas/eventos.js
@@ -7,54 +7,68 @@ var teclas =
 };
 
 console.log(teclas)
-
 document.addEventListener("keydown", dibujarTeclado);
 
+//variables del marco del lienzo
+var d = document.getElementById("area_de_dibujo");
+var ancho = d.width;
+var largo = d.height;
+
+//variables del area del dibujo
 var cuadrito = document.getElementById("area_de_dibujo");
 var papel = cuadrito.getContext("2d");
-var x = 150;
-var y = 150;
+var x = ancho/2;
+var y = largo/2;
 
-dibujarLinea("red", 5, 149, 149, 151, 151, papel);
-
-function dibujarLinea(color, ancho, xinicial, yinicial, xfinal, yfinal, lienzo)
+//Función para dibujar
+function dibujarLinea(color, grosor_del_lienzo, xinicial, yinicial, xfinal, yfinal, lienzo)
 {
 	lienzo.beginPath();
 	lienzo.strokeStyle = color;
-	lienzo.lineWidth = ancho;
+	lienzo.lineWidth = grosor_del_lienzo;
 	lienzo.moveTo(xinicial, yinicial);
 	lienzo.lineTo(xfinal, yfinal);
 	lienzo.stroke();
 	lienzo.closePath();
 }
 
+//Marco del lienzo
+linea_ancho = ancho - 1
+linea_largo = largo - 1
+dibujarLinea("black", 5, 1, 1, 1, linea_largo, papel);
+dibujarLinea("black", 5, linea_ancho, 1, 1, 1, papel);
+dibujarLinea("black", 5, linea_ancho, 1, linea_ancho, linea_largo, papel);
+dibujarLinea("black", 5, linea_ancho, linea_largo, 1, linea_largo, papel);
+
+
+//Funcion para que me reconozca la letra y en base a eso me dibuje
 function dibujarTeclado(evento)
 {
 	var colorcito = "blue"
-	var anchito = 3
-	var movimiento = 1
+	var grosorcito = 3
+	var movimiento = 10
 
 	switch(evento.keyCode)
 	{
 		
 		case teclas.UP:
-			dibujarLinea(colorcito, anchito, x, y, x, y - movimiento, papel);
+			dibujarLinea(colorcito, grosorcito, x, y, x, y - movimiento, papel);
 			y = y - movimiento;
 		break;
 
 		case teclas.DOWN:
-			dibujarLinea(colorcito, anchito, x, y, x, y + movimiento, papel);
+			dibujarLinea(colorcito, grosorcito, x, y, x, y + movimiento, papel);
 			y = y + movimiento;
 		break;
 
 		case teclas.LEFT:
-			dibujarLinea(colorcito, anchito, x, y, x - movimiento, y, papel);
+			dibujarLinea(colorcito, grosorcito, x, y, x - movimiento, y, papel);
 			x = x - movimiento;
 	
 		break;
 
 		case teclas.RIGHT:
-			dibujarLinea(colorcito, anchito, x, y, x + movimiento, y, papel);
+			dibujarLinea(colorcito, grosorcito, x, y, x + movimiento, y, papel);
 			x = x + movimiento;
 		break;
 
