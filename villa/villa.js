092a33f4ca2f2c3d870c3a80c8465126a97fda95
aurcode <63828066+aurcode@users.noreply.github.com>
2020-05-30 09:08:16 -0400

    Add files via upload

--- villa/villa.js
+++ villa/villa.js
@@ -1,10 +1,43 @@
-var z;
+var vp = document.getElementById("villa");
+var papel = vp.getContext("2d");
 
-for(var i=0; i<50; i++)
+var fondo = new Image();
+fondo.src = "tile.png";
+fondo.addEventListener("load", dibujar);
+
+var vaca = new Image();
+vaca.src = "vaca.png";
+vaca.addEventListener("load", dibujarVacas);
+
+var cerdo = new Image();
+cerdo.src = "cerdo.png";
+cerdo.addEventListener("load", dibujarCerdos);
+
+var pollo = new Image();
+pollo.src = "pollo.png";
+vaca.addEventListener("load", dibujarPollos);
+
+function dibujar()
 {
-	z = aleatorio(-100, 1000);
-	document.write(z + ", ");
+	papel.drawImage(fondo, 0, 0);
 }
+
+function dibujarVacas()
+{
+	papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarCerdos()
+{
+	papel.drawImage(cerdo, 10, 300);
+}
+
+function dibujarPollos()
+{
+	papel.drawImage(pollo, 300, 150);
+}
+
+
 
 function aleatorio(min, maxi)
 {
